7ab4f604628099c21ac32ecfd0fec0ca9c139cbf
Michal Zima <xhire@mujmalysvet.cz>
2011-06-27 19:02:23 +0200

    [me] New plugin that tells who I am

---
+++ plugins/me.rb
@@ -0,0 +1,25 @@
+#####
+## RubIRCot
+###
+#> plugin/me.rb
+#~ Plugin that tells who I am
+####
+# Author: Michal Zima, 2011
+# E-mail: xhire@mujmalysvet.cz
+#####
+
+class PluginMe
+  attr_reader :name
+  attr_reader :cmd
+  attr_reader :help
+
+  def initialize
+    @name = 'me'
+    @cmd  = 'me'
+    @help = 'tells who I am'
+  end
+
+  def run channel, params = ''
+    $bot.put "PRIVMSG #{channel} :ACTION RubIRCot v#{$version} - friendly IRC bot written in Ruby :c)"
+  end
+end
